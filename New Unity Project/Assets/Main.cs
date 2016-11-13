3e33eb655458b826f3dc1f6e029be33a09022126
Simona <simona.alexandra.paulescu@gmail.com>
2016-11-13 11:22:04 +0000

    Upload assets

---
+++ New Unity Project/Assets/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public int[,] matrix;
+	public GameObject prefab;
+	void Start() {
+		matrix = new int[,] {	
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+			{ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+			{ 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
+			{ 0, 0, 0, 0, 1, 1, 0, 1, 0, 0 },
+			{ 0, 0, 0, 0, 1, 1, 0, 1, 0, 0 },
+			{ 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+		};
+
+		//GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		//cube.AddComponent<Rigidbody>();
+		//cube.transform.position = new Vector3(0, 0, 0);
+		//GameObject test = Instantiate(Resources.Load("PBL") as GameObject, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+
+		// Create the cells
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (matrix [i, j] == 1) {
+					GameObject test = Instantiate (Resources.Load ("Foo") as GameObject, new Vector3 (i * 5.4f, 2, j * 5.4f), Quaternion.identity) as GameObject;
+					if((i != 0 & i != 9) & (j != 0 & j != 9)){
+						foreach(Transform child in test.transform) {
+							if (child.name == "North") {
+								if (matrix [i + 1, j] == 1)
+									Destroy (child.gameObject);
+							} else if (child.name == "East") {
+								if (matrix [i, j - 1] == 1)
+									Destroy (child.gameObject);
+							} else if (child.name == "South") {
+								if (matrix [i - 1, j] == 1)
+									Destroy (child.gameObject);
+							} else if (child.name == "West") {
+								if (matrix [i, j + 1] == 1)
+									Destroy (child.gameObject);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
---
+++ New Unity Project/Assets/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ 
+public class Menu : MonoBehaviour {
+
+	public WWW httpRequester;
+	public string url = "149.170.217.248/vspace/api/info.json";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void LoadMod () {
+		/*fsSerializer test;
+		print ("sdf");
+		httpRequester = new WWW (url);
+
+		while (!httpRequester.isDone)
+			;
+
+		httpRequester.
+		*/
+	}
+}
